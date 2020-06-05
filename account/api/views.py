3c7a32a2af565d979d57d0c6f4bb8aff3d2ccd53
lgorczynski98 <lgorczynski98@gmail.com>
2020-06-05 15:41:21 +0200

    Fixed registration

--- account/api/views.py
+++ account/api/views.py
@@ -6,24 +6,27 @@ from account.api.serializers import RegistrationSerializer
 from account.api.serializers import AccountSerializer
 from account.models import Account
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import authentication_classes, permission_classes
 
 class AccountList(generics.ListAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
+
 class AccountDetail(generics.RetrieveUpdateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
+
 @api_view(['POST',])
+@authentication_classes([])
+@permission_classes([])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
         data = {}
         if serializer.is_valid():
             account = serializer.save()
-            data['response'] = 'succesfully registered a new user'
-            data['email'] = account.email
             data['username'] = account.username
             token = Token.objects.get(user=account).key
             data['token'] = token
