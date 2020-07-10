d23872f5f94698bf7147dfc78ee14cbc102b32b4
lgorczynski98 <lgorczynski98@gmail.com>
2020-07-10 22:34:32 +0200

    Changed receipt model; return and warranty dates to weeks and months periods

--- receipt/api/serializers.py
+++ receipt/api/serializers.py
@@ -5,4 +5,4 @@ class ReceiptSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Receipt
-        fields = ['id', 'title', 'shop_name', 'purchase_date', 'purchase_cost', 'return_time', 'warranty_time', 'image', 'thumbnail', 'owner']
+        fields = ['id', 'title', 'shop_name', 'purchase_date', 'purchase_cost', 'weeks_to_return', 'months_of_warranty', 'image', 'thumbnail', 'owner']
